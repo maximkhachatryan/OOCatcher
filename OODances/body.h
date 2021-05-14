037b0f06f98ede14abfb6d54b5be02e4db8fae1f
Maxim <maxim.khachatryan.2013@gmail.com>
2021-05-14 22:09:35 +0400

    Declared few body methods.

--- OODances/body.h
+++ OODances/body.h
@@ -20,6 +20,23 @@ public:
 	void rand_rotate();
 	void shift(double dx, double dy);
 
+	//Rotate body segments
+	void RotateLeftLeg(double ang) const;
+	void RotateLeftFoot(double ang) const;
+	void RotateRightFoot(double ang) const;
+	void RotateLeftHand(double ang) const;
+	void RotateRightHand(double ang) const;
+	void RotateBase(double ang) const;
+	void RotateNeck(double ang) const;
+
+	//Collect body connecting disconnected line endpoints 
+	void CollectByLeftFoot() const;
+	void CollectByRightFoot() const;
+	void CollectByLeftHand() const;
+	void CollectByRightHand() const;
+	void CollectByBase() const;
+	void CollectByNeck() const;
+
 private:
 	line0 left_leg, left_arm;
 	line6 right_leg, right_arm;
--- OODances/main.cpp
+++ OODances/main.cpp
@@ -5,9 +5,15 @@
 
 int main()
 {
-	point *p = new point(0, 0), *q = new point(1, 1);
+	point *p = new point(0, 0), *q = new point(5, 5);
 	line5 seg5(*p, *q);
 	line4 seg4(*p, *q);
+
+	seg5.rotate(10);
+	std::cout << seg5.get_x() << std::endl;
+	std::cout << seg5.get_y() << std::endl;
+	std::cout << seg5.end_x() << std::endl;
+	std::cout << seg5.end_y() << std::endl;
 
 	std::cout << &seg5 << std::endl;
 	std::cout << &seg4 << std::endl;
