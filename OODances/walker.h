8dccb2186ec97cc620dc0b4c14355342c0ef33bf
Maxim <maxim.khachatryan.2013@gmail.com>
2021-05-14 22:15:18 +0400

    Add body within the walker class declaration.

--- OODances/walker.h
+++ OODances/walker.h
@@ -3,10 +3,12 @@
 class walker;
 
 #include "walkerState.h"
+#include "body.h"
 
 class walker {
 private:
-	walkerState *_walkerState;
+	body* _body;
+	walkerState* _walkerState;
 public:
 	void do_next_step();
 	bool try_to_catch();
