21744dc48def97828e1d5434a8981887cdc1b6a5
Maxim <maxim.khachatryan.2013@gmail.com>
2021-05-24 01:07:00 +0400

    Added simple_body_builder as a concrete builder.

---
+++ OODances/simple_body_builder.cpp
@@ -0,0 +1,33 @@
+#include "simple_body_builder.h"
+
+simple_body_builder::simple_body_builder()
+{
+    Reset();
+}
+
+simple_body_builder::~simple_body_builder()
+{
+    delete simple_body;
+}
+
+void simple_body_builder::build_legs() const
+{
+    //this->simple_body->set_left_leg();
+    //this->simple_body->set_right_leg();
+}
+
+void simple_body_builder::build_arms() const
+{
+    //this->simple_body->set_left_arm();
+    //this->simple_body->set_rigft_arm();
+}
+
+void simple_body_builder::build_torso() const
+{
+    //this->simple_body->set_torso()
+}
+
+void simple_body_builder::Reset()
+{
+    this->simple_body = new body();
+}
---
+++ OODances/simple_body_builder.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "body_builder.h"
+#include "body.h"
+
+class simple_body_builder :
+    public body_builder
+{
+private:
+    body* simple_body;
+
+public:
+    simple_body_builder();
+    ~simple_body_builder();
+
+    void build_legs() const override;
+
+    void build_arms() const override;
+
+    void build_torso() const override;
+
+    void Reset();
+};
+
